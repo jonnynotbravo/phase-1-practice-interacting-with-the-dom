6dc45496cdd3ad666188bfb490fe2b7b97d95238
Aaron David <ajdavid128@gmail.com>
2022-10-14 09:53:26 -0500

    made ticker variable, number variable, plus varialbe for plus button and made counter increment

--- js/challenge.js
+++ js/challenge.js
@@ -3,22 +3,28 @@
 const form = document.querySelector('form');
 
 form.addEventListener('submit', (e) =>{
-
     e.preventDefault();
-
     createComment(e.target['comment-input'].value);
-
-
+    form.reset();
 })
 
 function createComment(value){
-
     const comment = document.createElement('p');
-
     comment.textContent = value;
-
-    form.appendChild(comment);
-
-    
+    form.appendChild(comment);    
 }
 
+const ticker = document.querySelector('#counter');
+
+let number = Number(ticker.textContent);
+
+const plus = document.querySelector('#plus');
+
+plus.addEventListener('click', (e) =>{
+plusClick();
+console.log('ouch!');
+})
+
+function plusClick(){
+   ticker.textContent = ++number;
+};
